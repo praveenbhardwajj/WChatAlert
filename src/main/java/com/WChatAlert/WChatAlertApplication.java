23b02d23a0a93ce0e24141a3f217c29e53a5d2bc
Praveen Bhardwaj <praveenbhardwaj@gmail.com>
2026-01-31 11:24:01 +0530

    Intial Spring boot Project setup

---
+++ src/main/java/com/WChatAlert/WChatAlertApplication.java
@@ -0,0 +1,13 @@
+package com.WChatAlert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WChatAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WChatAlertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/WChatAlert/WChatAlertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WChatAlert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WChatAlertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
